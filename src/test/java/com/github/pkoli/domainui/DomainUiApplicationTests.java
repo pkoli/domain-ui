4c3424adef5635d8a34638c84b377b7f37943001
Pavan Shankar Koli <pskoli93@gmail.com>
2018-11-19 01:18:08 +0530

    Initial Commit

---
+++ src/test/java/com/github/pkoli/domainui/DomainUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pkoli.domainui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DomainUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
